22dc76ef338bd33580299a704f096962adb5d888
Rock de Vocht <peter@peter.co.nz>
2017-05-10 22:11:26 +1200

    added static resource get

--- server.py
+++ server.py
@@ -3,7 +3,8 @@
 import os
 import uuid
 
-from flask import Flask
+import kai.res
+from flask import Flask, Response
 from flask import request
 from kai.parser.parser import Parser
 from kai.sl.json_serialiser import JsonSystem
@@ -27,6 +28,80 @@ min_password_length = int(security_config['min_password_length'])
 app = Flask(__name__)
 parser = Parser()
 
+#########################################################################################
+# Static page server
+
+
+@app.route('/', methods=['GET'])
+@app.route('/index.html', methods=['GET'])
+@app.route('/login.html', methods=['GET'])
+@app.route('/query.html', methods=['GET'])
+@app.route('/entities.html', methods=['GET'])
+@app.route('/favicon.ico', methods=['GET'])
+def index():  # pragma: no cover
+    page = request.url.split('/')[-1]
+    mime_type = 'text/html'
+    if len(page) == 0:
+        page = 'index.html'
+    if page.endswith(".ico"):
+        mime_type = "image/x-icon"
+        content = open(kai.res.filename('web/' + page), 'rb').read()
+    else:
+        content = open(kai.res.filename('web/' + page)).read()
+    return Response(content, mimetype=mime_type)
+
+
+@app.route('/css', defaults={'path': ''})
+@app.route('/css/<path:path>')
+def get_css(path):  # pragma: no cover
+    complete_path = kai.res.filename('web/css/' + path)
+    if os.path.exists(complete_path):
+        content = open(complete_path).read()
+        return Response(content, mimetype="text/css")
+    else:
+        return Response("resource not found", status=404)
+
+@app.route('/fonts', defaults={'path': ''})
+@app.route('/fonts/<path:path>')
+def get_fonts(path):  # pragma: no cover
+    font_mime_types = {'svg': 'image/svg+xml', 'ttf': 'font/ttf',
+                       'woff': 'application/x-font-woff',
+                       'woff2': 'application/x-font-woff'}
+    complete_path = kai.res.filename('web/fonts/' + path)
+    if os.path.exists(complete_path):
+        content = open(complete_path).read()
+        mime_type = font_mime_types[path.split('.')[1]]
+        return Response(content, mimetype=mime_type)
+    else:
+        return Response("resource not found", status=404)
+
+
+@app.route('/images', defaults={'path': ''})
+@app.route('/images/<path:path>')
+def get_images(path):  # pragma: no cover
+    complete_path = kai.res.filename('web/images/' + path)
+    if os.path.exists(complete_path):
+        content = open(complete_path, 'rb').read()
+        mime_type = 'image/png'
+        if path.endswith('.jpg'):
+            mime_type = 'image/jpeg'
+        return Response(content, mimetype=mime_type)
+    else:
+        return Response("resource not found", status=404)
+
+
+@app.route('/js', defaults={'path': ''})
+@app.route('/js/<path:path>')
+def get_js(path):  # pragma: no cover
+    complete_path = kai.res.filename('web/js/' + path)
+    if os.path.exists(complete_path):
+        content = open(complete_path).read()
+        return Response(content, mimetype='application/javascript')
+    else:
+        return Response("resource not found", status=404)
+
+
+#########################################################################################
 
 # return an error rmessage with result code
 def return_error(error_str: str, status: int =500):
